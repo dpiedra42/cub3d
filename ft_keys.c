75db67372322951c169853b36f8456a15319bb57
deanna <42.dpiedra@gmail.com>
2020-06-30 15:08:34 +0200

    added ESC

--- ft_keys.c
+++ ft_keys.c
@@ -6,25 +6,33 @@
 /*   By: deannapiedra <deannapiedra@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/25 17:49:33 by deannapiedr       #+#    #+#             */
-/*   Updated: 2020/06/27 17:28:22 by deannapiedr      ###   ########.fr       */
+/*   Updated: 2020/06/30 15:08:02 by deannapiedr      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void	ft_leftright(int s, t_pos *pos, t_map *map)
+void	ft_close(int b, t_data *data)
+{
+	if (b == 1)
+		mlx_destroy_window(data->mlx_ptr, data->mlx_win);
+	free(data->mlx_ptr);
+	exit(0);
+}
+
+void	ft_leftright(int b, t_pos *pos, t_map *map)
 {
 	double mspeed;
 
 	mspeed = 0.5;
-	if (s == -1)
+	if (b == -1)
 	{
 		if (worldmap[(int)(pos->x)][(int)(pos->y + map->dir_x * mspeed)] == 0)
 			pos->y += map->dir_x * mspeed;
 		if (worldmap[(int)(pos->x - map->dir_y * mspeed)][(int)(pos->y)] == 0)
 			pos->x -= map->dir_y * mspeed;
 	}
-	if (s == 1)
+	if (b == 1)
 	{
 		if (worldmap[(int)(pos->x)][(int)(pos->y - map->dir_x * mspeed)] == 0)
 			pos->y -= map->dir_x * mspeed;
@@ -33,19 +41,19 @@ void	ft_leftright(int s, t_pos *pos, t_map *map)
 	}
 }
 
-void	ft_forback(int s, t_pos *pos, t_map *map)
+void	ft_forback(int b, t_pos *pos, t_map *map)
 {
 	double mspeed;
 
 	mspeed = 0.5;
-	if (s == -1)
+	if (b == -1)
     {
 		if(worldmap[(int)(pos->x + map->dir_x * mspeed)][(int)pos->y] == 0)
 			pos->x += map->dir_x * mspeed;
 		if(worldmap[(int)(pos->x)][(int)(pos->y + map->dir_y * mspeed)] == 0)
 			pos->y += map->dir_y * mspeed;
     }
-    if (s == 1)
+    if (b == 1)
     {
 		if(worldmap[(int)(pos->x - map->dir_x * mspeed)][(int)(pos->y)] == 0)
 			pos->x -= map->dir_x * mspeed;
@@ -54,7 +62,7 @@ void	ft_forback(int s, t_pos *pos, t_map *map)
 	}
 }
 
-void	ft_rotate(int s, t_map *map, t_pos *pos)
+void	ft_rotate(int b, t_map *map, t_pos *pos)
 {
 	double oldDirX;
 	double oldPlaneX;
@@ -63,10 +71,10 @@ void	ft_rotate(int s, t_map *map, t_pos *pos)
 	oldDirX = map->dir_x;
 	oldPlaneX = pos->plane_x;
 	rspeed = 0.05;
-	map->dir_x = map->dir_x * cos(s * rspeed) - map->dir_y * sin(s * rspeed);
-	map->dir_y = oldDirX * sin(s * rspeed) + map->dir_y * cos(s * rspeed);
-	pos->plane_x = pos->plane_x * cos(s * rspeed) - pos->plane_y * sin(s * rspeed);
-	pos->plane_y = oldPlaneX * sin(s * rspeed) + pos->plane_y * cos(s * rspeed);
+	map->dir_x = map->dir_x * cos(b * rspeed) - map->dir_y * sin(b * rspeed);
+	map->dir_y = oldDirX * sin(b * rspeed) + map->dir_y * cos(b * rspeed);
+	pos->plane_x = pos->plane_x * cos(b * rspeed) - pos->plane_y * sin(b * rspeed);
+	pos->plane_y = oldPlaneX * sin(b * rspeed) + pos->plane_y * cos(b * rspeed);
 }
 
 int		ft_keypress(int key, t_all *all)
@@ -83,7 +91,7 @@ int		ft_keypress(int key, t_all *all)
 		ft_rotate(-1, all->map, all->pos);
 	else if (key == RIGHT)
 		ft_rotate(1, all->map, all->pos);
+	else if (key == ESC)
+		ft_close(1, all->data);
 	return (1);
 }
-	// else if (key == ESC)
-	// 	ft_close(key, all);
