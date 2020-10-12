fb48d2e940fed0ee6f1096949cbc1b354513dd7d
deanna <42.dpiedra@gmail.com>
2020-10-12 12:26:52 +0200

    fixed format: space to tab

--- ft_parse_sprite.c
+++ ft_parse_sprite.c
@@ -5,46 +5,46 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: deannapiedra <deannapiedra@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/09/28 19:34:02 by deannapiedr       #+#    #+#             */
-/*   Updated: 2020/10/05 16:30:41 by deannapiedr      ###   ########.fr       */
+/*   Created: 2020/10/08 17:33:33 by deannapiedr       #+#    #+#             */
+/*   Updated: 2020/10/08 17:43:52 by deannapiedr      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-t_spr       *fill_spr(t_all *all, t_spr *sprites)
+t_spr	*fill_spr(t_all *all, t_spr *sprites)
 {
-    int i;
-    int x;
-    int y;
+	int i;
+	int x;
+	int y;
 
-    x = 0;
-    i = 0;
-    while (x < all->map->max_y)
-    {
-        y = 0;
-        while (y < all->map->max_x)
-        {
-            if (all->map->worldmap[x][y] == 2)
-            {
-                sprites[i].x = x;
-                sprites[i++].y = y;
-            }
-            y++;
-        }
-        x++;
-    }
-    return (sprites);
+	x = 0;
+	i = 0;
+	while (x < all->map->max_y)
+	{
+		y = 0;
+		while (y < all->map->max_x)
+		{
+			if (all->map->worldmap[x][y] == 2)
+			{
+				sprites[i].x = x;
+				sprites[i++].y = y;
+			}
+			y++;
+		}
+		x++;
+	}
+	return (sprites);
 }
 
-int			count_sprites(t_map *map)
+int		count_sprites(t_map *map)
 {
 	int i;
 	int j;
 	int spr;
 
 	i = 0;
-    spr = 0;
+	spr = 0;
 	while (i < map->max_y)
 	{
 		j = 0;
@@ -52,20 +52,20 @@ int			count_sprites(t_map *map)
 		{
 			if (map->worldmap[i][j] == 2)
 				spr++;
-            j++;
+			j++;
 		}
 		i++;
 	}
-    return (spr);
+	return (spr);
 }
 
-t_spr       *start_sprites(t_all *all)
+t_spr	*start_sprites(t_all *all)
 {
-    t_spr   *sprites;
-    
-    all->data->sprite->sprnum = count_sprites(all->map);
-    all->data->sprite->sprorder = malloc(sizeof(int) * (all->data->sprite->sprnum));
-    sprites = malloc(sizeof(t_spr) * all->data->sprite->sprnum);
-    sprites = fill_spr(all, sprites);
-    return (sprites);
+	t_spr   *sprites;
+
+	all->data->sprite->sprnum = count_sprites(all->map);
+	all->data->sprite->sprorder = malloc(sizeof(int) * (all->data->sprite->sprnum));
+	sprites = malloc(sizeof(t_spr) * all->data->sprite->sprnum);
+	sprites = fill_spr(all, sprites);
+	return (sprites);
 }
